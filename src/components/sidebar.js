35798d1e548ba00ca07e9ab84a51e5edfc82f13f
Akash4095 <akashathnure40@gmail.com>
2023-05-15 16:56:28 +0530

    removed framer motion

--- src/components/sidebar.js
+++ src/components/sidebar.js
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { AnimatePresence, motion } from "framer-motion";
 import { Icon } from "semantic-ui-react";
 import { routes } from "./sidebarRoutes";
 
@@ -11,55 +10,34 @@ const SideBar = ({ children }) => {
   const toggle = () => setIsOpen(!isOpen);
 
 
-  const showAnimation = {
-    hidden: {
-      width: 0,
-      opacity: 0,
-      transition: {
-        duration: 0.8,
-      },
-    },
-    show: {
-      opacity: 1,
-      width: "auto",
-      transition: {
-        duration: 0.8,
-      },
-    },
-  };
-
   return (
     <>
       <div className="main-container">
-        <motion.div
-          animate={{
-            width: isOpen ? "15vw" : "45px",
-            transition: { duration: 0.8, },
-          }}
-          className={`sidebar`}
-        >
-          <div className="top_section">
-            <AnimatePresence>
-              {isOpen && (
-                <motion.h1
-                  variants={showAnimation}
-                  initial="hidden"
-                  animate="show"
-                  exit="hidden"
-                  className="logo"
-                >
-                  NastedSidebar
-                </motion.h1>
-              )}
-            </AnimatePresence>
-
-            <div className="bars">
+        <div style={{ width: isOpen ? "16vw" : "45px", }} className={`sidebar`}>
+          <div className="top_section" >
+            <div className="link_text" style={{ display: isOpen ? "block" : "none", }}> {" Nasted Sidebar"}</div>
+            <div className="bars" style={{ marginLeft: isOpen ? "60px" : "0px" }}>
               <Icon className="icon" size="large" name="bars" onClick={toggle}></Icon>
             </div>
           </div>
-        
+
           <section className="routes">
             {routes.map((route, index) => {
+              if (route.subRoutes) {
+                return (
+                  <div className="menu">
+                    <div className="menu_items">
+                      <Icon className="icon" size="large" name={route.icon} title={route.name}></Icon>
+                      <div className="link_text" style={{ display: isOpen ? "block" : "none", }}>
+                        {route.name}
+                      </div>
+                    </div>
+                    <div>
+                      <Icon name="angle down"></Icon>
+                    </div>
+                  </div>
+                )
+              }
               return (
                 <NavLink
                   to={route.path}
@@ -68,24 +46,14 @@ const SideBar = ({ children }) => {
                   activeClassName="active"
                 >
                   <Icon className="icon" size="large" name={route.icon} title={route.name}></Icon>
-                  <AnimatePresence>
-                    {isOpen && (
-                      <motion.div
-                        variants={showAnimation}
-                        initial="hidden"
-                        animate="show"
-                        exit="hidden"
-                        className="link_text"
-                      >
-                        {route.name}
-                      </motion.div>
-                    )}
-                  </AnimatePresence>
+                  <div className="link_text" style={{ display: isOpen ? "block" : "none" }}>
+                    {route.name}
+                  </div>
                 </NavLink>
               );
             })}
           </section>
-        </motion.div>
+        </div>
 
         <main>{children}</main>
       </div>
